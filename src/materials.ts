936cbb6c41ee44e2801df8db889623b7475a9683
Deixel <Deixel@users.noreply.github.com>
2023-03-12 17:28:34 +0000

    Add all materials

--- src/materials.ts
+++ src/materials.ts
@@ -1,3 +1,4 @@
+import { maxHeaderSize } from "http";
 import { Artefact } from "./artefacts"
 class Material {
     name: string;
@@ -24,20 +25,50 @@ let Materials: {[name: string]: Material} = {
     vellum: new Material("Vellum"),
     leatherScraps: new Material("Leather Scraps"),
     soapstone: new Material("Soapstone"),
+    animalFurs: new Material("Animal Furs"),
+    fossilisedBone: new Material("Fossilied Bone"),
+
+    // Armadyl
+    stormguardSteel: new Material("Stormguard Steel"),
+    wingsofWar: new Material("Wings of War"),
+    armadyleanYellow: new Material("Armadylean Yellow"),
+    aetheriumAlloy: new Material("Aetherium Allow"),
+    quintessence: new Material("Quintessence"),
+
+    // Bandos
+    malachiteGreen: new Material("Malachite Green"),
+    markOfTheKyzaj: new Material("Mark of the Kyzaj"),
+    vulcanisedRubber: new Material("Vulcanised Rubber"),
+    warforgedBronze: new Material("Warforged Bronze"),
+    yubiuskClay: new Material("Yu'biusk Clay"),
+
+    // Dragonkin
+    compassRose: new Material("Compass Rose"),
+    felt: new Material("Felt"),
+    dragonMetal: new Material("Dragon Metal"),
+    carbonBlack: new Material("Carbon Black"),
+    orgone: new Material("Orgone"),
+
+    // Saradomin
+    keramos: new Material("Keramos"),
+    whiteMarble: new Material("White Marble"),
+    cobaltBlue: new Material("Cobalt Blue"),
+    everlightSilvthril: new Material("Everlight Silvthril"),
+    starOfSaradomin: new Material("Star of Saradomin"),
+
+    // Zamorak
+    cadmiumRed: new Material("Cadmium Red"),
+    chaoticBrimstone: new Material("Chaotic Brimstone"),
+    demonhide: new Material("Demonhide"),
+    eyeOfDagon: new Material("Eye of Dagon"),
+    hellfireMetal: new Material("Hellfire Metal"),
 
     // Zaros
     zarosianInsignia: new Material("Zarosian Insignia"),
     tyrianPurple: new Material("Tyrian Purple"), 
     imperialSteel: new Material("Imperial Steel"), 
     ancientVis: new Material("Ancient Vis"), 
-
-    // Zamorak
-
-    // Bandos
-
-    // Dragonkin
-
-    // Saradomin
+    bloodOfOrcus: new Material("Blood of Orcus"),
 }
 
 export {Materials, Material}
